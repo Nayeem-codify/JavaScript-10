f164de9b2cfbe502faa513da3eab9f7a77732eec
Nayeem-condify <webdevlopment2306@gmail.com>
2024-04-27 15:55:04 +0600

    js-10

---
+++ script.js
@@ -0,0 +1,14 @@
+let heading = document.querySelector(".heading")
+let inputbox = document.querySelector(".inputbox")
+let inputboxs = document.querySelector(".inputboxs")
+
+console.log( heading.innerHTML = "<a herf = '#' > JavaScript <a/> ");
+
+inputbox.placeholder = "Enter Your Email";
+inputbox.setAttribute("type" , "Email")
+
+
+inputboxs.placeholder = "Enter Your Password";
+inputboxs.setAttribute("type" , "password")
+
+
